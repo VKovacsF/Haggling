974cac7ddd1141959087b1123421a95902398d90
Protsentyyangel <a.protsentyy@students.htl-leonding.ac.at>
2026-04-21 11:52:22 +0200

    new project with updated dll

---
+++ HagglingShared/HagglingShared/Broking/Negotiation.cs
@@ -0,0 +1,26 @@
+﻿using HagglingShared.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HagglingShared.Broking
+{
+    public class Negotiation
+    {
+        public bool IsFinished { get; private set; }
+        public int MaxRounds { get; }
+
+        private int _currentRound = 0;
+
+        public Negotiation(int maxRounds = 10) => MaxRounds = maxRounds;
+
+        public void Start() { IsFinished = false; _currentRound = 0; }
+
+        public (HaggleOffer offer, int round) RunRound(HaggleOffer incoming)
+        {
+            _currentRound++;
+            if (_currentRound >= MaxRounds) IsFinished = true;
+            return (incoming, _currentRound);
+        }
+    }
+}
---
+++ HagglingShared/HagglingShared/Models/Haggle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HagglingShared.Models
+{
+        public record HaggleOffer(decimal Price, bool IsWalkingAway);
+
+        public record HaggleResult(HaggleOutcome Outcome, decimal? FinalPrice, int RoundsPlayed);
+
+        public enum HaggleOutcome
+        {
+            Deal,
+            VendorRefused,
+            CustomerWalkedAway,
+            MaxRoundsReached
+        }
+    }
